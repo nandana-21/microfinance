b7777fddba4b0df9c155721387f556820fdca2bb
nandana <nandana.tiwari.99@gmail.com>
2019-07-01 14:09:41 +0530

    hello

--- mainloan.cpp
+++ mainloan.cpp
@@ -1,8 +1,15 @@
 #include "mainloan.hpp"
+#include <string>
+using namespace eosio;
+using namespace std;
 
-void loan::leader_to_member(){
+void ACTION add_borrower(name acc_name, uint64_t b_id, vector<string> location,
+                    uint64_t b_phone, uint64_t group_type, asset loan_individual,
+                    asset b_balance, uint64_t group_id, uint64_t credit_score)
+{
   
 
-}
 
-EOSIO_DISPATCH(loan, (leader_to_member))
+}
+///namespace eosio
+EOSIO_DISPATCH(main_loan, (lender_to_contract))
--- mainloan.hpp
+++ mainloan.hpp
@@ -1,51 +1,55 @@
 #include <eosiolib/asset.hpp>
 #include <eosiolib/eosio.hpp>
+#include <string>
 
 using namespace eosio;
 using namespace std;
 
-CONTRACT loan : public contract{
+CONTRACT main_loan : public contract{
   private:
     //table definitions
 
     TABLE borrower_info{
       name acc_name;
-      uint256_t b_id; //hash
-      datastream <const char *> location; //geohash or coordinates
+      uint64_t b_id; //hash
+      vector <string> location; //geohash or coordinates
       uint64_t b_phone;
       uint64_t group_type;
       asset loan_individual;
-      uint256_t group_id; //hash function
+      asset b_balance; //borrower account balance
+      uint64_t group_id; //hash function
       uint64_t credit_score;
 
       auto primary_key()const {
         return acc_name.value;
       }
-    }
-
-    TABLE group_info{ //group leader should init this table ;;;;; primary key=group id
-      uint256_t group_id;
-      asset total_loan; //total loan of group
-      name acc_name; //group leader name
-      datastream<const char*> member_names; //name is ended with '/' //hash of this is the group_id
-
-      uint256_t primary_key() const{
+      uint64_t by_group_id() const{
         return group_id;
       }
-    }
+    };
 
-    TABLE shg_savings{
-      uint256_t group_id;
+    TABLE group_info{ //group leader should init this table ;;;;; primary key=group id
+      uint64_t group_id;
+      asset total_loan; //total loan of group
+      vector <string> member_names; //name is ended with '/' //hash of this is the group_id //CHANGE: make array //DONE
+
+      uint64_t primary_key() const{
+        return group_id;
+      }
+    };
+
+    TABLE shg_savings{ //NOT CONSIDERING
+      uint64_t group_id;
       uint64_t saving_amnt;
 
       uint64_t primary_key() const{
         return group_id;
       }
-    }
+    };
 
     TABLE underwriter_info{
       name acc_name;
-      uint256_t acc_id;
+      uint64_t acc_id;
       asset balance;
       uint64_t value_score;
 
@@ -53,32 +57,32 @@ CONTRACT loan : public contract{
       auto primary_key() const{
         return acc_name;
       }
-    }
+    };
 
     TABLE lender_info{
       name acc_name;
-      uint256_t acc_id;
+      uint64_t acc_id;
       asset balance;
 
       auto primary_key() const{
         return acc_name;
       }
-    }
+    };
 
     TABLE relayer_info{
       name acc_name;
-      uint256_t acc_id;
+      uint64_t acc_id;
       asset balance;
 
       auto primary_key() const{
         return acc_name;
       }
-    }
+    };
 
     TABLE loan_info{ //loan id needed to put
       name acc_name;
       asset lending_amount;
-      uint256_t lent_group_id;
+      uint64_t lent_group_id;
       uint64_t interest_rate;
       uint64_t payment_time;
       uint64_t status=0; //0-incomplete 1-complete
@@ -86,22 +90,45 @@ CONTRACT loan : public contract{
       auto primary_key() const{
         return acc_name;
       }
-    }
+    };
 
     //typedefs
-    typedef eosio::multi_index<"borrower"_n, borrower_info> borrower;
+    typedef eosio::multi_index<"borrower"_n, borrower_info,
+                                eosio::indexed_by<"groupId"_n,
+                                                  const_mem_fun<borrower_info, uint64_t, &borrower_info::by_group_id>>> borrower; //check
     typedef eosio::multi_index<"group"_n, group_info> group;
-    typedef eosio::multi_index<"shg"_n, shg_savings> shg;
+    typedef eosio::multi_index<"shg"_n, shg_savings> shg; //NOT CONSIDERING
     typedef eosio::multi_index<"underwriter"_n, underwriter_info> underwriter;
     typedef eosio::multi_index<"relayer"_n, relayer_info> relayer;
     typedef eosio::multi_index<"lender"_n, lender_info> lender;
     typedef eosio::multi_index<"loan"_n, loan_info> loan;
 
+    //init
+    borrower borr_table;
+    group group_table;
+    shg shg_table;
+    underwriter uwr_table;
+    relayer relayer_table;
+    lender lender_table;
+    loan loan_table;
+
+
+    vector <string> location; //geohash or coordinates
+    uint64_t b_phone;
+    uint64_t group_type;
+    asset loan_individual;
+    asset b_balance; //borrower account balance
+    uint64_t group_id; //hash function
+    uint64_t credit_score;
+
+
 
   public:
     using contract::contract;
 
-    ACTION leader_to_member();
+    ACTION add_borrower(name acc_name, uint64_t b_id, vector<string> location,
+                        uint64_t b_phone, uint64_t group_type, asset loan_individual,
+                        asset b_balance, uint64_t group_id, uint64_t credit_score);
 
 
-}
+};
