e5f666cac45f38d396328718b1e60b765114bdb5
nandana <nandana.tiwari.99@gmail.com>
2019-07-02 16:02:38 +0530

    lo

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # microfinance
 heeloooooooooooo
 HIiiiiiii
+
+front end:
+-when borrower is making an account:
+  |will be asked if its a new group or part of an existing group
+    -if new group, new group id will be generated which he would be asked to share with his group members
+    -if existing, he will input the generated group id.
--- mainloan.cpp
+++ mainloan.cpp
@@ -11,7 +11,6 @@ void mainloan::addborrower(name acc_name, uint64_t b_id, string location,
     eosio::check(b_balance.amount >= 0, "balance must be positive");
     eosio::check(credit_score>=300 && credit_score<=900, "credit score is always postive and ranges from 300-900");
 
-
     borr_table.emplace(get_self(), [&](auto& b) {
         b.acc_name = acc_name;
         b.b_id = b_id;
@@ -22,76 +21,96 @@ void mainloan::addborrower(name acc_name, uint64_t b_id, string location,
         b.group_id = group_id;
         b.credit_score = credit_score;
   });
+  makegroup(acc_name);
 }
 
-void mainloan::getborrower(name acc_name){
+void mainloan::makegroup(name acc_name){
+  require_auth(_self);
 
-void mainloan::addgroupinfo(uint64_t group_id, asset total_loan, vector <string> member_names)
-{
-    print("Adding group info", acc_name);
-    require_auth( _self );
+  auto borrower = borr_table.get(acc_name.value);
+  auto itr = group_table.find(borrower.group_id);
 
-      check(total_loan.amount >= 0, "loan must be positive");
+  if (itr==group_table.end()){
+    group_table.emplace(get_self(), [&](auto& g){
+      g.group_id = borrower.group_id;
+      g.total_loan += borrower.loan_individual;
+      g.member_names.push_back(borrower.acc_name.to_string());
+    });
+  }
+  else{
+    group_table.modify(itr, get_self(), [&](auto& g){
+      g.member_names.push_back(acc_name.to_string());
+    });
+  }
+}
 
-      group_table.emplace(get_self(), [&](auto& g) {
-          g.group_id = group_id;
-          g.total_loan = total_loan;
-          g.member_names = member_names;  //check once
-});
+void mainloan::adduwr(name acc_name, uint64_t acc_id, asset balance){ //not working; same error repeating for all tables below
+  require_auth(_self);
 
-void mainloan::addunderwriter(name acc_name, uint64_t acc_id,
+  uwr_table.emplace(get_self(), [&](auto& u){
+    u.acc_name = acc_name;
+    u.acc_id = acc_id;
+    u.balance = balance;
+  });
+
+}
+
+/*
+//COMMENTED BECAUSE NOT WORKING
+void mainloan::adduwr(name acc_name, uint64_t acc_id,
                                 asset balance, uint64_t value_score)
 {
     print("Adding underwriter", acc_name);
     require_auth( _self );
 
-      check(balance.amount >= 0, "balance must be positive");
-      check(value_score>=300 && value_score<=900, "credit score is always postive and ranges from 300-900");
+      eosio::check(balance.amount >= 0, "balance must be positive");
+      eosio::check(value_score>=300 && value_score<=900, "credit score is always postive and ranges from 300-900");
 
 
-      uwr_table.emplace(get_self(), [&](auto& l) {
+      uwr_table.emplace(get_self(), [&](auto& u) {
           u.acc_name = acc_name;
           u.acc_id = acc_id;
           u.balance = balance;
           u.value_score = value_score;
-});
+      });
+}
 
 void mainloan::addrelayer(name acc_name, uint64_t acc_id, asset balance)
 {
     print("Adding relayer", acc_name);
     require_auth( _self );
 
-      check(balance.amount >= 0, "balance must be positive");
+      eosio::check(balance.amount >= 0, "balance must be positive");
 
-      uwr_table.emplace(get_self(), [&](auto& r) {
+      relayer_table.emplace(get_self(), [&](auto& r) {
           r.acc_name = acc_name;
           r.acc_id = acc_id;
           r.balance = balance;
-});
-
+      });
+}
 
 void mainloan::addlender(name acc_name, uint64_t acc_id, asset balance)
 {
   print("Adding lender", acc_name);
   require_auth( _self );
 
-    check(balance.amount >= 0, "balance must be positive");
+    eosio::check(balance.amount >= 0, "balance must be positive");
 
     lender_table.emplace(get_self(), [&](auto& l) {
         l.acc_name = acc_name;
         l.acc_id = acc_id;
         l.balance = balance;
-});
-
-
+    });
+}*/
+/*
 void mainloan::addloaninfo(name acc_name, asset lending_amount, uint64_t lent_group_id,
-                          uint64_t interest_rate, uint64_t payment_time, uint64_t status)
+                          uint64_t interest_rate, uint64_t payment_time)
 {
   print("Adding loan info", acc_name);
   require_auth( _self );
 
     check(lending_amount.amount >= 0, "amount must be positive");
-    check(interest_rate.amount >= 0, "interest rate must be positive");
+    check(interest_rate >= 0, "interest rate must be positive");
 
     loan_table.emplace(get_self(), [&](auto& a) {
         a.acc_name = acc_name;
@@ -99,13 +118,13 @@ void mainloan::addloaninfo(name acc_name, asset lending_amount, uint64_t lent_gr
         a.lent_group_id = lent_group_id;
         a.interest_rate = interest_rate;
         a.payment_time = payment_time;
-        a.status = status;
-});
+    });
+}*/
 
 void mainloan::getborrower(name acc_name){
 
   auto borrower = borr_table.get(acc_name.value);
-  eosio::check(borrower.acc_name==acc_name, "Borrower doesn't exist.\n");
+  eosio::check(borrower.acc_name==acc_name, "Borrower doesn't exist.");
 
   print("Borrower Details: ", borrower.acc_name);
   print("ID: ", borrower.b_id);
@@ -118,19 +137,5 @@ void mainloan::getborrower(name acc_name){
 
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 ///namespace eosio
-EOSIO_DISPATCH(mainloan, (addborrower)(addgroupinfo)(addunderwriter)(addrelayer)(addlender)(addloaninfo))
+EOSIO_DISPATCH(mainloan, (addborrower)(adduwr)(makegroup)(getborrower))
